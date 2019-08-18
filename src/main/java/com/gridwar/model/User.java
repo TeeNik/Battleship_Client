862e356c6a4f8cf5fd0d6952ac8d4681478e9817
HimmelSpark <adam404pet@gmail.com>
2019-08-18 13:25:45 +0300

    for deployement

--- src/main/java/com/gridwar/model/User.java
+++ src/main/java/com/gridwar/model/User.java
@@ -3,10 +3,13 @@ package com.gridwar.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.web.socket.WebSocketSession;
 
 @AllArgsConstructor
 public class User {
+    @NotNull
     @Getter @Setter private String IMEI;
+    @NotNull
     @Getter @Setter private WebSocketSession session;
 }
